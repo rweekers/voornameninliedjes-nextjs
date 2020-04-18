bf917572afea356920a99f03fde3c2280a7c6591
Remco Weekers <remco.weekers@gmail.com>
2020-04-18 09:32:38 +0200

    Add photo details (WiP)

--- pages/song/[id].js
+++ pages/song/[id].js
@@ -1,4 +1,5 @@
 import Layout from '../../components/MyLayout';
+import axios from "axios";
 import Markdown from 'react-markdown';
 import fetch from 'isomorphic-unfetch';
 
@@ -14,22 +15,22 @@ const Post = props => (
         <iframe src={`https://www.youtube-nocookie.com/embed/${props.song.youtube}?rel=0`} width="100%" height="100%" title={props.song.title}></iframe>
       </aside>
       <aside className="song-photos">
-        {/* {hasWikiPhoto ? ( */}
+        {props.hasWikiPhoto ? (
           <div>
-            <img
-              src={props.song.artistImage} alt={props.song.artist}
-            />
-            <div className="attribution"><p>{props.song.artist}</p></div>
-          </div>
-        {/* ) : (
+              <img
+                src={props.wikiPhotoUrl} alt={props.song.artist}
+              />
+              <div className="attribution"><p>{props.wikiPhotoAttribution}</p></div>
+            </div>
+        ) : (
             <div>
               <img
-                src={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_c.jpg`}
-                alt={photo.title}
+                src={`https://farm${props.photo.farm}.staticflickr.com/${props.photo.server}/${props.photo.id}_${props.photo.secret}_c.jpg`}
+                alt={props.photo.title}
               />
-              <div className="attribution"><a href={contribution.photoUrl} target="_blank" rel="noopener noreferrer">Photo</a> by <a href={contribution.ownerUrl} target="_blank" rel="noopener noreferrer">{contribution.ownerName}</a> / <a href={contribution.licenseUrl} target="_blank" rel="noopener noreferrer">{contribution.licenseName}</a></div>
+              <div className="attribution"><a href={props.contribution.photoUrl} target="_blank" rel="noopener noreferrer">Photo</a> by <a href={props.contribution.ownerUrl} target="_blank" rel="noopener noreferrer">{props.contribution.ownerName}</a> / <a href={props.contribution.licenseUrl} target="_blank" rel="noopener noreferrer">{props.contribution.licenseName}</a></div>
             </div>
-          )} */}
+          )}
       </aside>
     </div>
     <style jsx>{`
@@ -141,13 +142,65 @@ const Post = props => (
 );
 
 Post.getInitialProps = async function (context) {
+  const API = 'https://api.voornameninliedjes.nl/songs/';
+  const FLICKR_PHOTO_DETAIL = 'https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=9676a28e9cb321d2721e813055abb6dc&format=json&nojsoncallback=true&photo_id=';
+  const FLICKR_USER_DETAIL = 'https://api.flickr.com/services/rest/?method=flickr.people.getInfo&api_key=9676a28e9cb321d2721e813055abb6dc&format=json&nojsoncallback=true&user_id=';
+  const FLICKR_LICENCES = 'https://api.flickr.com/services/rest/?method=flickr.photos.licenses.getInfo&api_key=9676a28e9cb321d2721e813055abb6dc&format=json&nojsoncallback=true'
+
+  let hasWikiPhoto = false;
+  let wikiPhotoUrl = '';
+  let wikiPhotoAttribution = '';
+
+  let contribution = {
+    "ownerName": '',
+    "ownerUrl": '',
+    "photoTitle": '',
+    "photoUrl": '',
+    "licenseName": '',
+    "licenseUrl": ''
+  }
+
+  let photo;
+  let owner;
+
   const { id } = context.query;
   const res = await fetch(`https://api.voornameninliedjes.nl/songs/${id}`);
   const song = await res.json();
 
+  if (song.wikimediaPhotos.length > 0) {
+    const wikiPhoto = song.wikimediaPhotos[0];
+
+    hasWikiPhoto = true;
+    wikiPhotoUrl = wikiPhoto.url;
+    wikiPhotoAttribution = wikiPhoto.attribution;
+  } else {
+    axios.get(FLICKR_PHOTO_DETAIL + song.flickrPhotos[0])
+      .then(res => {
+        photo = res.data.photo;
+        axios.get(FLICKR_USER_DETAIL + photo.owner.nsid)
+          .then(res => {
+            owner = res.data.person;
+            axios.get(FLICKR_LICENCES)
+              .then(res => {
+                const licenses = res.data.licenses.license;
+                const license = licenses.find(x => x.id === photo.license);
+                const licenseName = license.name;
+                const licenseUrl = license.url;
+
+                contribution.ownerName = owner.username._content;
+                contribution.ownerUrl = owner.photosurl._content;
+                contribution.photoTitle = photo.title._content;
+                contribution.photoUrl = photo.urls.url[0]._content;
+                contribution.licenseName = licenseName;
+                contribution.licenseUrl = licenseUrl;
+              })
+          })
+      })
+  }
+
   console.log(`Fetched song: ${song.title}`);
 
-  return { song };
+  return { song, hasWikiPhoto, wikiPhotoUrl, wikiPhotoAttribution, contribution, photo, owner };
 };
 
 export default Post;
