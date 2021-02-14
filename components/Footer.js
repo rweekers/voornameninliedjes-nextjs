4cb6042385a7adb56ef5e3a15ded4011c4309199
Remco Weekers <remco.weekers@gmail.com>
2021-02-14 10:59:21 +0100

    Update footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="app-footer">
       <p></p>
-      <div><h1>&copy; 2020 OrangeFlamingo </h1><a id="footerText" href="http://foo.bar">e-mail</a></div><a href="https://freebsd.org" target="blank"><img src="/freebsd-logo.png" className="freebsd-logo" alt="logo-freebsd" /></a>
+      <div><h1>&copy; 2021 OrangeFlamingo </h1><a id="footerText" href="http://foo.bar">e-mail</a></div><a href="https://freebsd.org" target="blank"><img src="/freebsd-logo.png" className="freebsd-logo" alt="logo-freebsd" /></a>
       <style jsx>{`
 .app-footer {
   height: 20px;
