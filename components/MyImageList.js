87c7d6e7df1982f81802b9f0f90bfae992efadf6
Remco Weekers <remco@craftsmen.nl>
2021-10-19 09:30:03 +0200

    Separate component for imageList

---
+++ components/MyImageList.js
@@ -0,0 +1,47 @@
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import Link from 'next/link';
+import IconButton from '@mui/material/IconButton';
+
+export default function Songrow(props) {
+
+  return (
+    <div className="image-list">
+      <ImageList variant={props.variant} cols={props.cols} sx={props.sx} gap={6}>
+        {props.songs.map((song) => (
+            <div className="detailSong" key={song.id}>
+              <Link href="/[artist]/[title]" className="test" as={`/${encodeURIComponent(song.artist.replace('?', '').replace('/', '').toLowerCase())}/${encodeURIComponent(song.title.toLowerCase())}`} passHref>
+                <ImageListItem key={song.id}>
+                  <img
+                    src={`${song.artistImage}?w=162&auto=format`}
+                    srcSet={`${song.artistImage}?w=162&auto=format&dpr=2 2x`}
+                    alt={song.title}
+                    loading="lazy"
+                  />
+                  <ImageListItemBar
+                    title={song.title}
+                    subtitle={song.artist}
+                  actionIcon={
+                    <IconButton
+                      sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                      aria-label={`info about ${song.title}`}
+                    >
+                      {/* <InfoIcon /> */}
+                    </IconButton>
+                  }
+                  />
+                </ImageListItem>
+              </Link>
+            </div>
+        ))}
+      </ImageList>
+      <style jsx>{`
+.image-list {
+
+}
+        `}
+      </style>
+    </div>
+  )
+}
